c7a17c1e41d4a67ad81f6ed6c26d44fb2fcf4dbc
Benjamin Rupp <github@benjaminrupp.de>
2021-08-27 10:25:01 +0200

    Introduce mock backend
    The nodemon config sets the node loader to ts-node/esm. This is
    necessary to run typescript scripts with node. The flat
    --experimental-specifier-resolution=node is needed to import typescript
    files inside the script. Otherwise, the imports need the .js file
    extension which is not understandable for the IDE.
    
    See https://github.com/TypeStrong/ts-node/issues/1007

---
+++ mocks/mockBackend.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { Room } from '../src/core/Room'
+
+const port = 3010
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/api/spaces/:spaceId/rooms', (req: any, res: any) => {
+	const rooms = [
+		new Room('Some Room'),
+		new Room('Top Secret Meeting'),
+		new Room('Lobby'),
+	]
+	res.send(rooms)
+})
+
+app.listen(port, () => {
+	console.log(`🤖 Mock backend is listening at http://localhost:${port}`)
+})
